0e61143c571b299f801f9d7a13c3893c6c02a6fa
Rauka-pvl <mr.redle3@gmail.com>
2025-10-16 13:17:51 +0500

    Edit Green API Service

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Artisan;
 
-Route::post('/webhook/whatsapp', [WebhookController::class, 'handleIncoming'])->name('webhook.whatsapp');
+Route::get('/webhook/whatsapp', [WebhookController::class, 'handleIncoming'])->name('webhook.whatsapp');
 Route::get('/send-reminders', function () {
     return Artisan::call('reminders:send');
 });
