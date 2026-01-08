f0b4e94c01e9904fa52f2424a5d177aa141e30b3
Rauka-pvl <mr.redle3@gmail.com>
2026-01-08 16:13:52 +0500

    Mobile Notf

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     public function sendCode(Request $request)
     {
         $request->validate([
-            'phone' => 'required|string|regex:/^7\d{9}$/',
+            'phone' => 'required|string|regex:/^7\d{11}$/',
         ]);
 
         $phone = $request->phone;
